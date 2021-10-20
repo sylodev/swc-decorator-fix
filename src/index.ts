c230f9472f165476357b3eb4ae8ae6792c66bb55
sylv <ryan@sylver.me>
2021-10-20 23:28:40 +0800

    minor improvements

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { addHook } from "pirates";
 
+const pattern = /\n +@[A-z]+\(.*\)\n +[A-z]+ ?!?: ?(?<type>.*);/g;
+
 addHook(
   (code) => {
-    return code.replace(/ +@[A-z]+\(.*\)\n +[A-z]+ ?!?: ?(?<type>.*);/g, (match, type) => {
+    return code.replace(pattern, (match, type) => {
       const replaced = match.replace(type, `${type} = undefined`);
       return replaced;
     });
